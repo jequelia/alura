e51c0e6ca487e287f3ce770a478f7f366cbaff25
jequelia <jequeliasantana@gmail.com>
2024-03-22 20:11:22 -0300

    feat: Add findByIdAndStatus method to CourseRepository

--- src/main/java/cursos/alura/api/domain/course/CourseRepository.java
+++ src/main/java/cursos/alura/api/domain/course/CourseRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Page<Course> findAllByStatus(Pageable paginacao, Boolean status);
+
+    boolean existsByIdAndStatus(Long courseId, boolean status);
+
+    Optional<Course> findByIdAndStatus(Long courseId, boolean status);
 
 }
