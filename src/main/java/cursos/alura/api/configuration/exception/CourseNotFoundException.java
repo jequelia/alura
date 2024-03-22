fb4232f271c01b5d69ca704907112b9189f0f11f
jequelia <jequeliasantana@gmail.com>
2024-03-22 20:08:18 -0300

    fix: Add super constructor call in CourseNotFoundException class

--- src/main/java/cursos/alura/api/configuration/exception/CourseNotFoundException.java
+++ src/main/java/cursos/alura/api/configuration/exception/CourseNotFoundException.java
@@ -1,6 +1,7 @@
 package cursos.alura.api.configuration.exception;
 
 public class CourseNotFoundException extends RuntimeException {
-    public CourseNotFoundException(String s) {
+    public CourseNotFoundException(String message) {
+        super(message);
     }
 }
